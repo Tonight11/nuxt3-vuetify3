262935f3fc42b7b51e27a92fb468cc652cea52db
tonight11 <godofspeedman@gmail.com>
2023-03-28 02:36:40 +0300

    feat: support btn

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -4,16 +4,31 @@
 	<div class="log-in">
 		<img class="logo" src="~/assets/img/logo.svg" alt="logo" />
 		<h2 class="title">Log in to your account</h2>
-		<AuthFormItem />
+		<AuthFormItem class="log-in__form" />
+		<div class="help">
+			Can’t log in to your account?
+			<nuxt-link to="/help">Ask support</nuxt-link>
+		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
+	a {
+		font-weight: 600;
+		font-size: 14px;
+		color: #6941c6;
+		text-decoration: none;
+	}
 	.log-in {
 		max-width: 360px;
 		text-align: center;
 		width: 100%;
-		margin: 20% auto 0;
+		margin: 100px auto 0;
+		padding: 0 10px;
+
+		&__form {
+			margin-bottom: 32px;
+		}
 	}
 
 	.logo {
