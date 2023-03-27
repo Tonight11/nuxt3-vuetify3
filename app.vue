13b910c284c2ccf20c77f998ea71b6664f9c87b4
tonight11 <godofspeedman@gmail.com>
2023-03-28 01:23:43 +0300

    feat: adding img

---
+++ app.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>
+		<v-app>
+			<v-row>
+				<v-col>
+					<AuthForm />
+				</v-col>
+				<v-col>
+					<img class="img" src="~/assets/img/sec.jpg" alt="" />
+				</v-col>
+			</v-row>
+		</v-app>
+	</div>
+</template>
+
+<style lang="scss">
+	body {
+		height: 100vh;
+		width: 100%;
+		overflow: hidden;
+	}
+
+	.img {
+		height: 100%;
+		width: 100%;
+		object-fit: cover;
+	}
+</style>
