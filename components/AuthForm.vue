4b839fdccd48fb224b3c802b23823c285f389c9a
tonight11 <godofspeedman@gmail.com>
2023-03-28 02:31:09 +0300

    style: centered a form

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts"></script>
 
 <template>
-	<img src="~/assets/img/logo.svg" alt="logo" />
-	<v-toolbar-title>Log in to your account</v-toolbar-title>
-	<AuthFormItem />
+	<div class="log-in">
+		<img class="logo" src="~/assets/img/logo.svg" alt="logo" />
+		<h2 class="title">Log in to your account</h2>
+		<AuthFormItem />
+	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	.log-in {
+		max-width: 360px;
+		text-align: center;
+		width: 100%;
+		margin: 20% auto 0;
+	}
+
+	.logo {
+		margin-bottom: 24px;
+	}
+
+	.title {
+		margin-bottom: 32px;
+	}
+</style>
